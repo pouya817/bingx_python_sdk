3c8c37270ca6af49ac9bade9bba163d58aafa1ff
pouya817 <m.pouya.ch@gmail.com>
2024-05-21 18:13:12 +0330

    added new config

---
+++ config/config_dev.py
@@ -0,0 +1,8 @@
+# Endpoint bingx urls
+BASE_BINGX_URLS = 'https://open-api.bingx.com'
+SET_BINGX_ORDER_URLS = '/openApi/swap/v2/trade/order'
+SET_BINGX_LEVERAGE_URLS = '/openApi/swap/v2/trade/leverage'
+GET_BINGX_PRICE_URL = '/openApi/swap/v2/quote/price'
+GET_BINGX_POSITION_URL = '/openApi/swap/v2/user/positions'
+CLOSE_BINGX_ORDER_URL = '/openApi/swap/v2/trade/order'
+GET_BINGX_OPEN_ORDERS_URL = '/openApi/swap/v2/trade/order'
